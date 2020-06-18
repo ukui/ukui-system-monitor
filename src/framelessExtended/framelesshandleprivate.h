521038ba840c6e764c4c1fd2de7bbf825f2dbaf6
rxy <renxinyu@kylinos.cn>
2020-06-18 15:56:52 +0800

    add copyright for new files

--- src/framelessExtended/framelesshandleprivate.h
+++ src/framelessExtended/framelesshandleprivate.h
@@ -1,11 +1,12 @@
-/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
+/*
+ * Copyright (C) 2013 ~ 2018 National University of Defense Technology(NUDT) & Tianjin Kylin Ltd.
  *
- * Copyright (C) 2019 Tianjin KYLIN Information Technology Co., Ltd.
+ * Authors:
+ *  Kobe Lee    xiangli@ubuntukylin.com/kobe24_lixiang@126.com
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * the Free Software Foundation; version 3.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -13,9 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA.
- *
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #ifndef FRAMELESSHANDLEPRIVATE_H
 #define FRAMELESSHANDLEPRIVATE_H
